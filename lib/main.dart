ec20ef2ed53b0a65e8bc8f320b8e06f1e5a26d47
duktartamang <duktar13@gmail.com>
2023-07-25 15:26:26 +0545

    -

--- lib/main.dart
+++ lib/main.dart
@@ -43,7 +43,7 @@ class Smartdustbin extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: aboutUS(),
+      home: landingpage(),
     );
   }
 }
